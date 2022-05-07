41a33b0e841a90944fbf014f3ebf2e264fa70325
Warlock <internalmike@gmail.com>
2022-05-07 11:19:51 +0300

    ci

--- ensure_no_std/src/main.rs
+++ ensure_no_std/src/main.rs
@@ -1,9 +1,12 @@
+#![feature(const_ptr_offset_from)]
 #![feature(default_alloc_error_handler)]
 #![feature(start)]
 
 #![deny(warnings)]
 
 #![no_std]
+
+extern crate alloc;
 
 use core::alloc::Layout;
 use core::panic::PanicInfo;
@@ -38,88 +41,63 @@ pub fn rust_oom(_layout: Layout) -> ! {
     unsafe { exit(98) }
 }
 
-macro_attr! {
-    #[derive(Debug, Component!)]
-    struct ItemData {
-        props: ItemProps,
-    }
-}
+mod objs {
+    use dep_obj::dep_type;
+    use dep_obj::templates::detached_static_dep_type;
+    use alloc::borrow::Cow;
 
-macro_attr! {
-    #[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, NewtypeComponentId!)]
-    struct Item(Id<ItemData>);
-}
-
-impl DepObjId for Item { }
-
-dep_type_with_builder! {
-    #[derive(Debug)]
-    struct ItemProps become props in Item {
-        name: Cow<'static, str> = Cow::Borrowed(""),
-        equipped: bool = false,
-        enhancement: i8 = 0,
-    }
-
-    type BaseBuilder<'a> = ItemBuilder<'a>;
-}
-
-struct ItemBuilder<'a> {
-    item: Item,
-    state: &'a mut dyn State,
-}
-
-impl<'a> DepObjBaseBuilder<Item> for ItemBuilder<'a> {
-    fn id(&self) -> Item { self.item }
-    fn state(&self) -> &dyn State { self.state }
-    fn state_mut(&mut self) -> &mut dyn State { self.state }
-}
-
-impl<'a> ItemBuilder<'a> {
-    fn props(
-        self,
-        f: impl for<'b> FnOnce(ItemPropsBuilder<'b>) -> ItemPropsBuilder<'b>
-    ) -> Self {
-        f(ItemPropsBuilder::new_priv(self)).base_priv()
-    }
-}
-
-impl Item {
-    fn new(state: &mut dyn State) -> Item {
-        let game: &mut Game = state.get_mut();
-        game.items.insert(|id| (ItemData { props: ItemProps::new_priv() }, Item(id)))
-    }
-
-    fn drop_item(self, state: &mut dyn State) {
-        self.drop_bindings_priv(state);
-        let game: &mut Game = state.get_mut();
-        game.items.remove(self.0);
-    }
-
-    fn build<'a>(
-        self,
-        state: &'a mut dyn State,
-        f: impl FnOnce(ItemBuilder<'a>) -> ItemBuilder<'a>
-    ) {
-        f(ItemBuilder { item: self, state });
-    }
-
-    dep_obj! {
-        fn props(self as this, game: Game) -> (ItemProps) {
-            if mut {
-                &mut game.items[this.0].props
-            } else {
-                &game.items[this.0].props
-            }
+    dep_type! {
+        #[derive(Debug)]
+        pub struct ItemProps in Item {
+            name: Cow<'static, str> = Cow::Borrowed(""),
+            base_weight: f32 = 0.0,
+            weight: f32 = 0.0,
+            equipped: bool = false,
+            cursed: bool = false,
         }
     }
+
+    pub type Item = detached_static_dep_type::Id<ItemProps>;
+    pub type Objs = detached_static_dep_type::Arena<ItemProps>;
 }
 
+mod behavior {
+    use dep_obj::binding::Binding3;
+    use dyn_context::state::State;
+    use crate::objs::*;
+
+    pub fn new_item(state: &mut dyn State) -> Item {
+        let item = Item::new(state);
+        let weight = Binding3::new(state, (), |(), base_weight, cursed, equipped| Some(
+            if equipped && cursed { base_weight + 100.0 } else { base_weight }
+        ));
+        ItemProps::WEIGHT.bind(state, item.props(), weight);
+        weight.set_source_1(state, &mut ItemProps::BASE_WEIGHT.value_source(item.props()));
+        weight.set_source_2(state, &mut ItemProps::CURSED.value_source(item.props()));
+        weight.set_source_3(state, &mut ItemProps::EQUIPPED.value_source(item.props()));
+        return item;
+    }
+}
+
+use dep_obj::binding::{Bindings};
+use dyn_context::state::{State, StateRefMut};
+use objs::*;
+use behavior::*;
+
+fn run(state: &mut dyn State) {
+    let item = new_item(state);
+    ItemProps::BASE_WEIGHT.set(state, item.props(), 5.0).immediate();
+    ItemProps::CURSED.set(state, item.props(), true).immediate();
+    ItemProps::EQUIPPED.set(state, item.props(), true).immediate();
+    ItemProps::CURSED.set(state, item.props(), false).immediate();
+    item.drop_self(state);
+}
 
 #[start]
 pub fn main(_argc: isize, _argv: *const *const u8) -> isize {
-    let tree = &mut WidgetTree::new();
-    let widget = Widget::new(tree, tree.root());
-    assert_eq!(widget.parent(tree), Some(tree.root()));
-    widget.drop(tree);
+    (&mut Objs::new()).merge_mut_and_then(|state| {
+        run(state);
+        Objs::drop_self(state);
+    }, &mut Bindings::new());
     0
 }
